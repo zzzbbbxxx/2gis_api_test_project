c37d39bead0ca231d7d98681c06bcdb64e4971de
zzzbbbxxx <orehov.oreh@bk.ru>
2020-07-29 10:29:55 +0300

    add new test -> for "page_size" parameter, and fix last test -> for "page" parameter,

--- src/main/java/reqWithParamPage.java
+++ src/main/java/reqWithParamPage.java
@@ -15,24 +15,24 @@ import static org.testng.Assert.assertTrue;
 public class reqWithParamPage extends reqBase {
 
 
-        //This function will provide the patameter data
-        @DataProvider(name = "Data-Provider-Function_test10_0")
-        public Object[][] parameterTestProvider_test10_0() {
-        return new Object[][] {
+    //This function will provide the patameter data
+    @DataProvider(name = "Data-Provider-Function_test10_0")
+    public Object[][] parameterTestProvider_test10_0() {
+        return new Object[][]{
                 {"?page=-1"}
         };
-        }
+    }
 
-        // for page = -1
-        // ...status code = 200
-        // ...structure of json = errorscheme
-        @Test(dataProvider = "Data-Provider-Function_test10_0")
-        public void test10_0(String q)  {
+    // for page = -1
+    // ...status code = 200
+    // ...structure of json = errorscheme
+    @Test(dataProvider = "Data-Provider-Function_test10_0")
+    public void test10_0(String q) {
 
         HttpResponse<String> jsonResponse = sendRequestGetResponseString
-                (path,q);
+                (path, q);
 
-        if (!checkStatus(200,jsonResponse.getStatus())) {
+        if (!checkStatus(200, jsonResponse.getStatus())) {
 
         } else {
             JSONObject jsonExpected = getJSONfromJSONFile("page\\json_example_for_page_param_with_error_2.json");
@@ -42,108 +42,105 @@ public class reqWithParamPage extends reqBase {
             boolean validation = validationSchema
                     ("page\\param_page_code_error_schema_2.json",
                             jsonObject);
-            assertTrue(validation,"Response must be equal ErrorSchema,\n"
-                    +"Response Expected: "+ jsonExpected+"\n"
-                    +"Responce Actual: "+ jsonObject);
+            assertTrue(validation, "Response must be equal ErrorSchema,\n"
+                    + "Response Expected: " + jsonExpected + "\n"
+                    + "Responce Actual: " + jsonObject);
         }
 
+    }
+
+
+    //This function will provide the patameter data
+    @DataProvider(name = "Data-Provider-Function_test10")
+    public Object[][] parameterTestProvider_test10() {
+        return new Object[][]{
+                {"?page"},
+                {"?page="},
+                {"?page=0"},
+                {"?page=1.5"},
+                {"?page=one"}
+        };
+    }
+
+    // for page = empty, 0, 1.5, one
+    // ...status code = 200
+    // ...structure of json = errorscheme
+    @Test(dataProvider = "Data-Provider-Function_test10")
+    public void test10(String q) {
+
+        HttpResponse<String> jsonResponse = sendRequestGetResponseString
+                (path, q);
+
+        if (!checkStatus(200, jsonResponse.getStatus())) {
+
+        } else {
+            JSONObject jsonExpected = getJSONfromJSONFile("page\\json_example_for_page_param_with_error.json");
+
+            JSONObject jsonObject = new JSONObject(jsonResponse.getBody());
+
+            boolean validation = validationSchema
+                    ("page\\param_page_code_error_schema.json",
+                            jsonObject);
+            assertTrue(validation, "Response must be equal ErrorSchema,\n"
+                    + "Response Expected: " + jsonExpected + "\n"
+                    + "Responce Actual: " + jsonObject);
         }
 
-
-        //This function will provide the patameter data
-        @DataProvider(name = "Data-Provider-Function_test10")
-        public Object[][] parameterTestProvider_test10() {
-            return new Object[][] {
-                    {"?page"},
-                    {"?page="},
-                    {"?page=0"},
-                    {"?page=1.5"},
-                    {"?page=one"}
-                    };
-        }
-
-        // for page = empty, 0, 1.5, one
-        // ...status code = 200
-        // ...structure of json = errorscheme
-        @Test(dataProvider = "Data-Provider-Function_test10")
-        public void test10(String q)  {
-
-                HttpResponse<String> jsonResponse = sendRequestGetResponseString
-                        (path,q);
-
-                if (!checkStatus(200,jsonResponse.getStatus())) {
-
-                } else {
-                        JSONObject jsonExpected = getJSONfromJSONFile("page\\json_example_for_page_param_with_error.json");
-
-                        JSONObject jsonObject = new JSONObject(jsonResponse.getBody());
-
-                        boolean validation = validationSchema
-                                ("page\\param_page_code_error_schema.json",
-                                        jsonObject);
-                        assertTrue(validation,"Response must be equal ErrorSchema,\n"
-                        +"Response Expected: "+ jsonExpected+"\n"
-                        +"Responce Actual: "+ jsonObject);
-                }
-
-        }
+    }
 
 
+    //This function will provide the patameter data
+    @DataProvider(name = "Data-Provider-Function-test11")
+    public Object[][] parameterTestProvider_test11() {
+        return new Object[][]{
+                {"?page=1", ""}
+        };
+    }
 
-         //This function will provide the patameter data
-         @DataProvider(name = "Data-Provider-Function-test11")
-         public Object[][] parameterTestProvider_test11() {
-            return new Object[][] {
-                    {"?page=1", ""}
-                    };
-        }
+    // ответ для page=1 == ответу для запроса без page
+    @Test(dataProvider = "Data-Provider-Function-test11")
+    public void test11(String q1, String q2) {
 
-        // ответ для page=1 == ответу для запроса без page
+        HttpResponse<String> jsonResponse1 = sendRequestGetResponseString
+                (path, q1);
 
-        @Test(dataProvider = "Data-Provider-Function-test11")
-        public void test11(String q1, String q2) {
+        HttpResponse<String> jsonResponse2 = sendRequestGetResponseString
+                (path, q2);
 
-                HttpResponse<String> jsonResponse1 = sendRequestGetResponseString
-                        (path, q1);
+        JSONObject jsonExpected = getJSONfromJSONFile("/json_for_base_schema.json");
 
-                HttpResponse<String> jsonResponse2 = sendRequestGetResponseString
-                        (path, q2);
+        JSONObject jsonObject1 = new JSONObject(jsonResponse1.getBody());
+        JSONObject jsonObject2 = new JSONObject(jsonResponse2.getBody());
 
-                JSONObject jsonExpected = getJSONfromJSONFile("/json_for_base_schema.json");
+        boolean validation = validationSchema
+                ("/base_schema.json",
+                        jsonObject1);
 
-                 JSONObject jsonObject1 = new JSONObject(jsonResponse1.getBody());
-                 JSONObject jsonObject2 = new JSONObject(jsonResponse2.getBody());
-
-                boolean validation = validationSchema
-                        ("/base_schema.json",
-                                jsonObject1);
-
-                if (validation)
-                    assertEquals(jsonObject1.toString(), jsonObject2.toString(),
-                            "Response for \"?page=1\" must be equal response without params,\n"
-                        + "Response \"?page=1\": " + jsonObject1 + "\n"
-                        + "Responce without param: " + jsonObject2);
+        if (validation)
+            assertEquals(jsonObject1.toString(), jsonObject2.toString(),
+                    "Response for \"?page=1\" must be equal response without params,\n"
+                            + "Response \"?page=1\": " + jsonObject1 + "\n"
+                            + "Responce without param: " + jsonObject2);
 
 
-        }
+    }
 
 
+    //"Перелистывание" страниц / у
+    @Test
+    public void test12() {
 
-        //"Перелистывание" страниц / у
-        @Test
-        public void test12() {
+        List<String> pages = Arrays.asList("?page=1", "?page=2", "?page=3");
+        List<String> listOfRegions = new ArrayList<>();
+        int _page = 0;
 
-            List<String> pages = Arrays.asList("?page=1","?page=2","?page=3");
-            List<String> listOfRegions =  new ArrayList<>();
-            int _page = 0;
+        for (String q : pages) {
 
-            for (String q : pages) {
-
-                _page++;
+            _page++;
             HttpResponse<String> jsonResponse = sendRequestGetResponseString
-                    (path,q);
+                    (path, q);
 
-            if (!checkStatus(200,jsonResponse.getStatus())) {
+            if (!checkStatus(200, jsonResponse.getStatus())) {
 
             } else {
 
@@ -151,42 +148,45 @@ public class reqWithParamPage extends reqBase {
 
                 JSONArray tmpObj = jsonObject.getJSONArray("items");
 
-                List<String> _listOfRegions =  new ArrayList<>();
+                List<String> _listOfRegions = new ArrayList<>();
 
 
                 JSONObject mJsonObject = new JSONObject();
 
-                for (int i = 0; i < tmpObj.length() ; i++) {
+                for (int i = 0; i < tmpObj.length(); i++) {
 
-                    mJsonObject = (JSONObject)tmpObj.get(i);
+                    mJsonObject = (JSONObject) tmpObj.get(i);
 
                     String name = mJsonObject.get("name").toString();
                     _listOfRegions.add(name);
 
                     Assert.assertTrue(arrayContains(name, listOfRegions),
-                            "List of regions before error: "+listOfRegions+"\n"
-                    +"Region: "+name+" is already was in response\n"
-                    +"Current page: "+q+"\n"
-                    +"List of regions on the errorPage: "+_listOfRegions+"\n");
+                            "List of regions before error: " + listOfRegions + "\n"
+                                    + "Region: " + name + " is already was in response\n"
+                                    + "Current page: " + q + "\n"
+                                    + "List of regions on the errorPage: " + _listOfRegions + "\n");
 
-                    if (i==5) listOfRegions.add("\n");
+                    if (i == 5) listOfRegions.add("\n");
 
 
                     listOfRegions.add(name);
                 }
-                listOfRegions.add(":page="+_page+" ");
+                listOfRegions.add(":page=" + _page + " ");
 
             }
-          }
         }
+    }
 
-        public boolean arrayContains(String value, List<String> arr ) {
-            for (String item : arr) {
-                if (item.equals(value)) {
-                    return false;
-                    }
-                }
-            return true;
+    public boolean arrayContains(String value, List<String> arr) {
+        for (String item : arr) {
+            if (item.equals(value)) {
+                return false;
+            }
         }
+        return true;
+    }
+
+
+
 
 }
--- src/main/java/reqWithParamPageSize.java
+++ src/main/java/reqWithParamPageSize.java
@@ -12,24 +12,52 @@ import static org.testng.Assert.assertTrue;
 
 public class reqWithParamPageSize extends reqBase {
 
+    //This function will provide the patameter data
+    @DataProvider(name = "Data-Provider-Function_test13")
+    public Object[][] parameterTestProvider_test13() {
+        return new Object[][]{
+                {""}
+        };
+    }
 
+    // page default = 15
+    // ...status code = 200
+    @Test(dataProvider = "Data-Provider-Function_test13")
+    public void test13(String q) {
+
+        HttpResponse<String> jsonResponse = sendRequestGetResponseString
+                (path, q);
+
+        if (!checkStatus(200, jsonResponse.getStatus())) {
+
+        } else {
+
+            JSONObject jsonObject = new JSONObject(jsonResponse.getBody());
+
+            JSONArray tmpObj = jsonObject.getJSONArray("items");
+
+            Assert.assertEquals(tmpObj.length(),15,"Count regions in response by default must be equel 15");
+
+
+        }
+    }
 
         //This function will provide the patameter data
-        @DataProvider(name = "Data-Provider-Function_test13")
-        public Object[][] parameterTestProvider_test13() {
+        @DataProvider(name = "Data-Provider-Function-test14")
+        public Object[][] parameterTestProvider_test14() {
             return new Object[][] {
                     {"?page_size"},
                     {"?page_size="},
                     {"?page_size=1.5"},
-                    {"?page_size=one"}
-                    };
+                    {"?page_size=one"},
+            };
         }
 
         // for page_size = empty, 0, 1.5, one
         // ...status code = 200
         // ...structure of json = errorscheme
-        @Test(dataProvider = "Data-Provider-Function_test13")
-        public void test13(String q)  {
+        @Test(dataProvider = "Data-Provider-Function-test14")
+        public void test14(String q)  {
 
                 HttpResponse<String> jsonResponse = sendRequestGetResponseString
                         (path,q);
@@ -53,36 +81,70 @@ public class reqWithParamPageSize extends reqBase {
 
 
          //This function will provide the patameter data
-         @DataProvider(name = "Data-Provider-Function_test14")
-         public Object[][] parameterTestProvider_test14() {
+         @DataProvider(name = "Data-Provider-Function-test15")
+         public Object[][] parameterTestProvider_test15() {
             return new Object[][] {
                     {"?page_size=0"},
                     {"?page_size=1"},
-                    {"?page_size=one"},
-                    {"?page_size=-1"},
+                    {"?page_size=-1"}
+
             };
             }
 
-        // for page_size = empty, 0, 1.5, one
+        // for page_size = 0, 1, -1
         // ...status code = 200
         // ...structure of json = errorscheme
-        @Test(dataProvider = "Data-Provider-Function_test14")
-        public void test14(String q)  {
+        @Test(dataProvider = "Data-Provider-Function-test15")
+        public void test15(String q)  {
             HttpResponse<String> jsonResponse = sendRequestGetResponseString
                 (path,q);
             if (!checkStatus(200,jsonResponse.getStatus())) {
                 } else {
-            JSONObject jsonExpected = getJSONfromJSONFile("page_size\\json_example_for_page_size_param_with_error.json");
+            JSONObject jsonExpected = getJSONfromJSONFile("page_size\\json_example_for_page_size_param_with_error2.json");
 
             JSONObject jsonObject = new JSONObject(jsonResponse.getBody());
 
             boolean validation = validationSchema
-                    ("page_size\\param_page_size_code_error_schema.json",
+                    ("page_size\\param_page_size_code_error_schema2.json",
                             jsonObject);
             assertTrue(validation,"Response must be equal ErrorSchema,\n"
                     +"Response Expected: "+ jsonExpected+"\n"
                     +"Responce Actual: "+ jsonObject);
             }
         }
+
+
+    //This function will provide the patameter data
+    @DataProvider(name = "Data-Provider-Function-test16")
+    public Object[][] parameterTestProvider_test16() {
+        return new Object[][] {
+                {"?page_size=5",5},
+                {"?page_size=10",10},
+                {"?page_size=15",15}
+
+        };
+    }
+
+    // for page_size = 5, 10, 15
+    // ...status code = 200
+    // ...structure of json = errorscheme
+    @Test(dataProvider = "Data-Provider-Function-test16")
+    public void test16(String q, int i)  {
+        HttpResponse<String> jsonResponse = sendRequestGetResponseString
+                (path,q);
+            if (!checkStatus(200, jsonResponse.getStatus())) {
+
+            } else {
+
+                JSONObject jsonObject = new JSONObject(jsonResponse.getBody());
+
+                JSONArray tmpObj = jsonObject.getJSONArray("items");
+
+                Assert.assertEquals(tmpObj.length(),i,"Count regions in response by default must be equel 15");
+
+
+            }
+        }
+
 
 }
