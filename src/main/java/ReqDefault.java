7b8dcaedd944fa80105f162c62cb617a3c5eab84
zzzbbbxxx <orehov.oreh@bk.ru>
2020-10-18 10:15:06 +0300

    исправил тест на проверку списка кодов: теперь это тест без параметров; теперь тест падает, если в ответе на запрос не хватает какого-то кода из "codeList"

--- src/main/java/ReqDefault.java
+++ src/main/java/ReqDefault.java
@@ -1,5 +1,3 @@
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
@@ -12,33 +10,39 @@ import static org.testng.Assert.assertEquals;
 
 public class ReqDefault extends ReqBase {
 
-        @Test(description= "page_size default must be contains 15 regions")
-        public void testDefaultPageSize() {
+        @Test(description = "country_code by default must be show all regions for all countries")
+        public void testDefaultCountryCode() {
 
-                int count = HelperReq.getCountOfRegions(PATH, "");
+                HashSet<String> codeSet = new HashSet<>();
+                for (String page :  Arrays.asList("?page_size=5&page=1",
+                        "?page_size=5&page=2", "?page_size=5&page=3",
+                        "?page_size=5&page=4", "?page_size=5&page=5",
+                        "?page_size=5&page=6")) {
 
-                Assert.assertEquals(count,
-                                15,
-                                "Count of regions in response by default must be equal: 15 \n");
+                        JSONArray tmpObj = HelperReq.getJsonArray(
+                                HelperReq.sendRequestGetJSON(PATH,page),
+                                "items");
+
+                        tmpObj.forEach(item -> {
+                                codeSet.add(HelperReq.getCountryCodeValue((JSONObject)item));
+                        });
+                }
+
+
+                List<String> codeList = Arrays.asList( "ru", "kg", "kz", "cz", "ua");
+                List<String> codeListFromSet = new ArrayList<String>(codeSet);
+
+                HelperReq.islistsEquals(codeList, codeListFromSet);
 
         }
 
 
-        @DataProvider(name = "Data-Provider-Function-test11")
-        public Object[][] parameterTestProvider_test11() {
-                return new Object[][]{
-                        {"?page=1", ""}
-                };
-        }
-
-
-        @Test(dataProvider = "Data-Provider-Function-test11",
-        description = "page by default must be equel page=1" )
+        @Test(description = "page by default must be equal page=1" )
         public void testDefaultPageNumber(String q1, String q2) {
 
 
-                org.json.JSONObject jsonObject1 = HelperReq.sendRequestGetJSON(PATH, q1);
-                org.json.JSONObject jsonObject2 = HelperReq.sendRequestGetJSON(PATH, q2);
+                org.json.JSONObject jsonObject1 = HelperReq.sendRequestGetJSON(PATH, "?page=1");
+                org.json.JSONObject jsonObject2 = HelperReq.sendRequestGetJSON(PATH, "");
 
                 HelperReq.validateSchema(BASE_SCHEMA,jsonObject1);
 
@@ -78,48 +82,19 @@ public class ReqDefault extends ReqBase {
 
 
 
-        //This function will provide the patameter data
-        @DataProvider(name = "Data-Provider-Function_test9")
-        public Object[][] parameterTestProvider_test9() {
-                return new Object[][] {
-                        {"?page_size=5&","page=1"},
-                        {"?page_size=5&","page=2"},
-                        {"?page_size=5&","page=3"},
-                        {"?page_size=5&","page=4"},
-                        {"?page_size=5&","page=5"},
-                        {"?page_size=5&","page=6"},
+        @Test(description= "page_size default must be contains 15 regions")
+        public void testDefaultPageSize() {
 
-                };
+                int count = HelperReq.getCountOfRegions(PATH, "");
+
+                Assert.assertEquals(count,
+                        15,
+                        "Count of regions in response by default must be equal: 15 \n");
+
         }
 
-        // country_code by default must be show all regions for all countries
-        // я мог написать тест двумя способами:
-        // исходя из документации - проверяя, что все страны из списка
-        // ... есть в ответе
-        // или исходя из знания, что в списке отсутствует "ua"
-        // в первом случае будет ошибка, потому что я ищу только страны из списка и "падаю",
-        // ...если появляется то, чего я не жду
 
-        @Test(dataProvider = "Data-Provider-Function_test9",
-        description = "...")
-        public void testDefaultCountryCode(String q1, String q2) {
 
-                JSONArray tmpObj = HelperReq.getJsonArray(
-                        HelperReq.sendRequestGetJSON(PATH,q1+q2),
-                        "items");
-
-                HashSet<String> codeSet = new HashSet<>();
-
-                tmpObj.forEach(item -> {
-                        codeSet.add(HelperReq.getCountryCodeValue((JSONObject)item));
-                });
-
-                List<String> codeList = Arrays.asList("ru", "kg",  "kz", "cz");
-                Assert.assertTrue(
-                        HelperReq.arrayContainsElemFrom(codeSet, codeList),
-                        "Code on this page: " + codeSet + "\n" +
-                                "Code by default must be in { ru, kg, kz, cz } \n ");
-        }
 
 
 }
